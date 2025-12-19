c4d5dff6a78604020fc69385e906da31804c8553
yakov-ecorun <vladimirr-polyakovv@gmail.com>
2025-12-19 19:31:35 +0200

    Add exit codes to log cleanup script

--- clean_logs.sh
+++ clean_logs.sh
@@ -3,15 +3,18 @@
 LOG="logs/monitor.log"
 MAX_SIZE=10485760  # 10 MB
 
-if [ -f "$LOG" ]; then
-    SIZE=$(stat -c%s "$LOG")
-
-    if [ "$SIZE" -gt "$MAX_SIZE" ]; then
-        echo "Log is too big, cleaning"
-        > "$LOG"
-    else
-        echo "Log size is OK"
-    fi
-else
+if [ ! -f "$LOG" ]; then
     echo "Log file not found"
+    exit 1
+fi
+
+SIZE=$(stat -c%s "$LOG")
+
+if [ "$SIZE" -gt "$MAX_SIZE" ]; then
+    echo "Log is too big, cleaning"
+    > "$LOG"
+    exit 0
+else
+    echo "Log size is OK"
+    exit 0
 fi
